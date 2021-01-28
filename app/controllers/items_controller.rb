1b0cac98902d2f1a47dab299cb55703fe455125e
ocha185 <kitsu11171107@gmail.com>
2021-01-28 12:12:31 +0900

    商品削除機能の修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,13 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.user_id == current_user.id
+      if item.destroy
+        redirect_to root_path
+      else
+        redirect_to root_path
+      end
+    end
   end
 
   private
